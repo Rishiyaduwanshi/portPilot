be0d1d1495264a366f70e0eba4b31c58a23f5d4e
Abhinav Prakash <abprakash85409@gmail.com>
2025-04-06 23:22:23 +0530

    Fix -> Port 80 already use in windows

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import { ensureDomainInHosts } from './lib/host.js';
 import { createProxyServer } from './lib/proxy/server.js';
 
 export async function startProxy(config) {
-  await ensureDomainInHosts(config.domain);
-  
-  await createProxyServer(config);
+  const domainAlreadyMapped = await ensureDomainInHosts(config.domain);
+  await createProxyServer(config, domainAlreadyMapped);
 }
